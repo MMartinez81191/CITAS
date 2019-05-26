a47dcf170f8c20594c30663ce97ae631b24a1bdc
mmartinez81191 <martin.francisco.martinez.f@gmail.com>
2019-05-26 15:37:17 -0700

    ACTUALIZACION DE BASE DE DATOS
    SE AGREGA ACTUALIZACION DE BASE DE DATOS

--- pinguino_citas.sql
+++ pinguino_citas.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 07-05-2019 a las 05:15:16
+-- Tiempo de generación: 27-05-2019 a las 00:36:06
 -- Versión del servidor: 10.1.37-MariaDB
 -- Versión de PHP: 7.2.12
 
@@ -72,7 +72,9 @@ CREATE TABLE `citas` (
 INSERT INTO `citas` (`id_cita`, `id_cliente`, `numero_turno`, `fecha`, `hora`, `costo_consulta`, `folio_sat`, `cobrado`, `contabilizado`, `activo`) VALUES
 (41, 7, 1, '2019-05-04', '13:00:00', '200', NULL, 1, 0, 1),
 (42, 7, 1, '2019-05-05', '13:00:00', '0', NULL, 0, 0, 1),
-(43, 7, 2, '2019-05-04', '13:15:00', '0', NULL, 0, 0, 1);
+(43, 7, 2, '2019-05-04', '13:15:00', '0', NULL, 0, 0, 1),
+(44, 7, 1, '2019-05-25', '19:15:00', '0', NULL, 0, 0, 1),
+(45, 7, 1, '2019-05-26', '14:45:00', '0', NULL, 0, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -103,7 +105,34 @@ INSERT INTO `clientes` (`id_cliente`, `nombre_cliente`, `telefono_cliente`, `cor
 (13, 'SDF', '66624162466', 'sdf@gmail.com', '2019-04-25', 1),
 (14, 'SDF', 'ASD', 'sdf@gmail.com', '2019-05-01', 1),
 (15, 'SDF', '637121416544', 'sdf@gmail.com', '2019-04-30', 1),
-(16, 'SDF', 'SDF', 'sdf@gmail.com', '2019-05-01', 1);
+(16, 'SDF', 'SDF', 'sdf@gmail.com', '2019-05-01', 1),
+(17, 'JOSE LOPEZ PEREX', '333333333333', 'sdf@gmail.com', '2019-05-29', 1),
+(18, 'MARTIN MARTINEZ', '6624162499', '', '2019-05-30', 1),
+(19, 'MARINA LARA DE MARTINEZ', '662123214645', '', '2019-02-18', 1),
+(20, 'PRUEBA EDAD', '6662', 'sdf@gmail.com', '1991-11-08', 1),
+(21, 'PRUEBA EDAD', '6662', 'sdf@gmail.com', '1991-11-08', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `costos`
+--
+
+CREATE TABLE `costos` (
+  `id_costo` int(11) NOT NULL,
+  `costo` varchar(45) DEFAULT NULL,
+  `activo` varchar(45) DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `costos`
+--
+
+INSERT INTO `costos` (`id_costo`, `costo`, `activo`) VALUES
+(11, '100', '1'),
+(12, '200', '1'),
+(13, '300', '1'),
+(14, '500', '1');
 
 -- --------------------------------------------------------
 
@@ -153,6 +182,12 @@ ALTER TABLE `clientes`
   ADD PRIMARY KEY (`id_cliente`);
 
 --
+-- Indices de la tabla `costos`
+--
+ALTER TABLE `costos`
+  ADD PRIMARY KEY (`id_costo`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -173,13 +208,19 @@ ALTER TABLE `cat_niveles`
 -- AUTO_INCREMENT de la tabla `citas`
 --
 ALTER TABLE `citas`
-  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+  MODIFY `id_cita` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- AUTO_INCREMENT de la tabla `clientes`
 --
 ALTER TABLE `clientes`
-  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `id_cliente` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT de la tabla `costos`
+--
+ALTER TABLE `costos`
+  MODIFY `id_costo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
