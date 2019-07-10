5b8926ffbd867720ffa94446876e185466138bda
mmartinez81191 <martin.francisco.martinez.f@gmail.com>
2019-07-09 20:32:53 -0700

    x
    x

--- application/models/CorteParcial_model.php
+++ application/models/CorteParcial_model.php
@@ -81,6 +81,7 @@ class CorteParcial_Model extends CI_Model {
         $this->db->where('cobrado',1);
         $this->db->where('activo',1);
         $this->db->where('contabilizado',0);
+        $this->db->where('costo_consulta','100');
         $this->db->where('fecha >=',$fecha_inicial);
         $this->db->where('fecha <=',$fecha_final);
         $this->db->order_by('id_cita','ASC');
