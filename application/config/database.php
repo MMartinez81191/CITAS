05fb6328a1a2a9f50db428d2a751bf6504a1f742
Martin Martinez <mtmartinez@interlogicglobal.com>
2020-05-01 22:58:12 -0700

    #12 Actualzacion de coniguracion para server local
    Se actualiza la configuracion de la base de datos para adecuarlo a localhost

--- application/config/database.php
+++ application/config/database.php
@@ -73,30 +73,30 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-// $db['default'] = array(
-//     'hostname' => 'pinguinosystems.com',
-//     'username' => 'pinguino_rosy',
-//     'password' => 'rosy123',
-//     'database' => 'pinguino_gspmconsultor',
-//     'dbdriver' => 'mysqli',
-//     'dbprefix' => '',
-//     'pconnect' => TRUE,
-//     'db_debug' => TRUE,
-//     'cache_on' => FALSE,
-//     'cachedir' => '',
-//     'char_set' => 'utf8',
-//     'dbcollat' => 'utf8_general_ci',
-//     'swap_pre' => '',
-//     'encrypt'  => FALSE,
-//     'compress' => FALSE,
-//     'stricton' => FALSE,
-//     'failover' => array()
-// );
-
-$db['default'] = array(
+/*$db['default'] = array(
     'hostname' => 'localhost',
     'username' => 'pinguino_citas',
     'password' => ']WyVB^@sSR(+',
+    'database' => 'pinguino_citas',
+    'dbdriver' => 'mysqli',
+    'dbprefix' => '',
+    'pconnect' => TRUE,
+    'db_debug' => TRUE,
+    'cache_on' => FALSE,
+    'cachedir' => '',
+    'char_set' => 'utf8',
+    'dbcollat' => 'utf8_general_ci',
+    'swap_pre' => '',
+    'encrypt'  => FALSE,
+    'compress' => FALSE,
+    'stricton' => FALSE,
+    'failover' => array()
+);*/
+
+$db['default'] = array(
+    'hostname' => 'localhost',
+    'username' => 'root',
+    'password' => '',
     'database' => 'pinguino_citas',
     'dbdriver' => 'mysqli',
     'dbprefix' => '',
